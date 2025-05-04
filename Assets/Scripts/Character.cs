abe69cb8fa424a49515080a64f4e5e568073f62b
ginger59620 <a59620@alunos.ipb.pt>
2025-05-04 22:33:28 +0100

    change the character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+[System.Serializable]
+public class Character
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sprite characterSprite;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
