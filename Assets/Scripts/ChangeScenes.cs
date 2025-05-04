35a9b57c291140834f5e58448bf270b446e70cc0
ginger59620 <a59620@alunos.ipb.pt>
2025-05-04 23:14:21 +0100

    done ig

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip Shine;
+
     public void GoToSceneOne()
     {
+        source.PlayOneShot(Shine);
         SceneManager.LoadScene("SceneOne");
     }
     public void GoToSceneTwo()
     {
+        source.PlayOneShot(Shine);
         SceneManager.LoadScene("SceneTwo");
     }
 
